462c8fb162c2846da3134d8c80a52e86f4691c4f
ashes999 <ashes999@yahoo.com>
2017-04-13 22:29:54 -0400

    Implement enum!

--- code/source/PlayState.hx
+++ code/source/PlayState.hx
@@ -10,25 +10,19 @@ import flixel.math.FlxRandom;
 
 class PlayState extends FlxState
 {
-	/// TODO: ENUM
-	public static inline var UP = "up";
-	public static inline var RIGHT = "right";
-	public static inline var DOWN = "down";
-	public static inline var LEFT = "left";
-
 	private static inline var NUM_GROUPS:Int = 3;
 	private static inline var GROUP_SIZE:Int = 1;
-	private static var ALL_TILES:Array<String> = [UP, RIGHT, DOWN, LEFT];
+	private static var ALL_TILES:Array<Tile> = [Tile.Up, Tile.Right, Tile.Down, Tile.Left];
 	private var random = new FlxRandom();
 
 	override public function create():Void
 	{
 		super.create();
 
-		var groups:Array<Array<String>> = [];
+		var groups:Array<Array<Tile>> = [];
 		for (i in 0 ... NUM_GROUPS) {
 			
-			var currentGroup = new Array<String>();
+			var currentGroup = new Array<Tile>();
 			for (j in 0 ... GROUP_SIZE) {
 				currentGroup.push(random.getObject(ALL_TILES));
 			}
@@ -42,4 +36,11 @@ class PlayState extends FlxState
 	{
 		super.update(elapsed);
 	}
+}
+
+enum Tile {
+	Up;
+	Right;
+	Down;
+	Left;
 }
